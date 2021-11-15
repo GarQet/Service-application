6ba4195c117d867ee3eb0d6c59e6082d17f1a29d
Robert Rachwal <xsalon@wp.pl>
2021-11-15 18:59:41 +0100

    LastThreeCases Controller fix

--- Service Application/force-app/main/default/aura/LastThreeCases/LastThreeCasesController.js
+++ Service Application/force-app/main/default/aura/LastThreeCases/LastThreeCasesController.js
@@ -1,42 +1,15 @@
 ({
-    getUserCases: function(component,event,helper){
-        component.set("v.loaded", !component.get("v.loaded"));
-        let load = component.get("v.currentUser.Id");
-        if(load != null) {
-            let action = component.get("c.getUserCasesData");
-            action.setParams({
-                user: component.get("v.currentUser"),
-                limitOfRecords: component.get("v.limitOfRecords")
-            });
-            action.setCallback(this, function(response){
-                console.debug(response);
-                let state = response.getState();
-                if (state === "SUCCESS") {
-                    component.set("v.ownerOfCases", response.getReturnValue());
-                } else {
-                    console.debug(response.error[0].message);
-                }
-            });
-            $A.enqueueAction(action);
-        }
+    getUserCases: function(component, event, helper){
+        component.set("v.loaded", true);
+		helper.getUserCases(component);
     },
-    hideRecords: function(component,event,helper){
-        component.set("v.loaded", !component.get("v.loaded"));
+    hideRecords: function(component, event, helper){
+        component.set("v.loaded", false);
     },  
-    gotoRecord : function(component,event,helper) {
-        let id;
-        let element = event.srcElement;
-        while(element.parentNode) {
-            if(element.id != "") {
-                id = element.id;
-                break;
-            }
-            element = element.parentNode;
-        }
-        if(id == null) { return; }
-        var sObjectEvent = $A.get("e.force:navigateToSObject");
+    goToRecord : function(component, event, helper) {
+        let sObjectEvent = $A.get("e.force:navigateToSObject");
         sObjectEvent.setParams({
-            "recordId": id,
+            "recordId": event.target.parentElement.id,
             "slideDevName": 'detail'
         });
         sObjectEvent.fire();
